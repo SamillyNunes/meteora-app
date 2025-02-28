d644e6f750141e62e6c75887f8a188936107071c
Samilly <samillynunes19@gmail.com>
2025-02-28 11:42:14 -0300

    draft: addProduct method created inside Produtos component while using useContext hook

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import Produto from "./Produto";
 import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
+import { CarrinhoContext } from "@/context/CarrinhoContext";
 
-const Produtos = ({ adicionarProduto }) => {
+const Produtos = () => {
+  const {cart, setCart} = useContext(CarrinhoContext);
+
+  const addProduct = (newProduct)=>{
+    const hasTheProduct = cart.some(c=> c.id===newProduct.id);
+
+    if(!hasTheProduct){
+      newProduct.quantidade=1;
+      return setCart([...cart, newProduct]);
+    }
+
+    // se o produto ja existir, vou apenas aumentar a quantidade de itens
+    setCart((oldCart)=>oldCart.map(cartItem => {
+      if(cartItem.id===newProduct.id){
+        cartItem.quantidade+=1;
+      }
+      return cartItem;
+    }));
+  }
+
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
@@ -12,7 +32,7 @@ const Produtos = ({ adicionarProduto }) => {
           <Produto
             key={produto.id}
             {...produto}
-            adicionarProduto={adicionarProduto}
+            adicionarProduto={addProduct}
           />
         ))}
       </div>
--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,10 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const CarrinhoContext = createContext();
 
 export const CarrinhoProvider = ({children}) => {
+    const [cart, setCart] = useState([]);
+
     return (
-        <CarrinhoContext.Provider>
+        <CarrinhoContext.Provider value={{
+            cart,
+            setCart,
+        }}>
             {children}
         </CarrinhoContext.Provider>
     );
