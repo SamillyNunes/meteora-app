42e5deae6c52660562d0352112f5c24690bbe853
Samilly <samillynunes19@gmail.com>
2025-03-06 15:57:38 -0300

    feat: using useReducer hook to optmize code

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,24 +1,48 @@
-import { createContext, useReducer, useState } from "react";
+import { createContext, useEffect, useMemo, useReducer, useState } from "react";
 import { cartReducer } from "../reducers/cartReducer";
 
 export const CarrinhoContext = createContext();
-CarrinhoContext.displayName = 'Carrinho';
+CarrinhoContext.displayName = "Carrinho";
 
 const initialState = [];
 
-export const CarrinhoProvider = ({children}) => {
-    const [cart, dispatch] = useReducer(cartReducer, initialState);
-    const [quantity] = useState(0);
-    const [total] = useState(0);
+export const CarrinhoProvider = ({ children }) => {
+  const [cart, dispatch] = useReducer(cartReducer, initialState);
+  const [quantity, setQuantity] = useState(0);
+  const [total, setTotal] = useState(0);
 
-    return (
-        <CarrinhoContext.Provider value={{
-            cart,
-            dispatch,
-            quantity,
-            total,
-        }}>
-            {children}
-        </CarrinhoContext.Provider>
+  // acumulator eh a var responsavel pelo valor reduzido em um, ou seja, o que esta acumulando
+  // o segundo parametro eh o valor inicial
+  // o uso do useMemo vai fazer com que ele guarde a informacao do calculo e mantenha ela ate
+  // que seja necessario calcular novamente
+  const { totalTemp, quantityTemp } = useMemo(() => {
+    return cart.reduce(
+      (accumulator, product) => ({
+        quantityTemp: accumulator.quantityTemp + product.quantidade,
+        totalTemp: accumulator.totalTemp + product.preco * product.quantidade,
+      }),
+      {
+        quantityTemp: 0,
+        totalTemp: 0,
+      }
     );
-}
+  }, [cart]);
+
+  useEffect(() => {
+    setQuantity(quantityTemp);
+    setTotal(totalTemp);
+  });
+
+  return (
+    <CarrinhoContext.Provider
+      value={{
+        cart,
+        dispatch,
+        quantity,
+        total,
+      }}
+    >
+      {children}
+    </CarrinhoContext.Provider>
+  );
+};
--- src/hooks/useCartContext.js
+++ src/hooks/useCartContext.js
@@ -1,78 +1,59 @@
 import { useContext, useEffect, useMemo } from "react";
 import { CarrinhoContext } from "@/context/CarrinhoContext";
+import { ADD_PRODUCT, REMOVE_PRODUCT, UPDATE_QUANTITY } from "../reducers/cartReducer";
+
+// funcoes para usar no reducer:
+const addProductAction = (newProduct) => {
+  return {
+    type: ADD_PRODUCT,
+    payload: newProduct
+  }
+}
+
+const removeProductAction = (productId) => {
+  return {
+    type: REMOVE_PRODUCT,
+    payload: productId,
+  }
+}
+
+const updateProductQuantityAction = (productId, quantity) => {
+  return {
+    type: UPDATE_QUANTITY,
+    payload: {
+      produtoId: productId,
+      quantidade: quantity,
+    }
+  }
+}
 
 // o fato do arquivo comecar com use, sinaliza para o react que isso sera hook customizado
 export const useCartContext = () => {
-  const { cart, setCart, quantity, setQuantity, total, setTotal } =
+  const { cart, dispatch , quantity, total } =
     useContext(CarrinhoContext);
 
-  const changeQuantity = (id, qty) => {
-    return cart.map((item) => {
-      if (item.id === id) item.quantidade += qty;
-
-      return item;
-    });
-  };
-
   const addProduct = (newProduct) => {
-    const hasTheProduct = cart.some((c) => c.id === newProduct.id);
-
-    if (!hasTheProduct) {
-      newProduct.quantidade = 1;
-      return setCart([...cart, newProduct]);
-    }
-
-    const updatedCart = changeQuantity(newProduct.id, 1);
-
-    // se o produto ja existir, vou apenas aumentar a quantidade de itens
-    setCart([...updatedCart]);
+    dispatch(addProductAction(newProduct));
   };
 
   const removeProduct = (productId) => {
-    const product = cart.find((c) => c.id === productId);
-
-    if (!product) return;
-
-    if (product.quantidade > 1) {
-      const updatedCart = changeQuantity(productId, -1);
-      return setCart([...updatedCart]);
+    const prod = cart.find(item => item.id ===productId);
+    
+    if(prod && prod.quantidade>1){
+      dispatch(updateProductQuantityAction(productId, prod.quantidade-1));
+    } else {
+      dispatch(removeProductAction(productId));
     }
-
-    const cartWithoutItem = cart.filter((p) => p.id !== productId);
-    setCart(cartWithoutItem);
   };
 
   const deleteCartProduct = (productId) => {
-    const products = cart.filter((item) => item.id !== productId);
-
-    setCart(products);
+    dispatch(removeProductAction(productId));
   };
 
-  // acumulator eh a var responsavel pelo valor reduzido em um, ou seja, o que esta acumulando
-  // o segundo parametro eh o valor inicial
-  // o uso do useMemo vai fazer com que ele guarde a informacao do calculo e mantenha ela ate
-  // que seja necessario calcular novamente
-  const { totalTemp, quantityTemp } = useMemo(() => {
-    return cart.reduce(
-      (accumulator, product) => ({
-        quantityTemp: accumulator.quantityTemp + product.quantidade,
-        totalTemp: accumulator.totalTemp + product.preco * product.quantidade,
-      }),
-      {
-        quantityTemp: 0,
-        totalTemp: 0,
-      }
-    );
-  }, [cart]);
-
-  useEffect(() => {
-    setQuantity(quantityTemp);
-    setTotal(totalTemp);
-  });
+  
 
   return {
     cart,
-    setCart,
     total,
     quantity,
     addProduct,
