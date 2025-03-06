b15fac8afa51a01d501d532b2f43edd48843d8e8
Samilly <samillynunes19@gmail.com>
2025-03-05 21:15:57 -0300

    chore: set carrinho context display name to view in react context when debugguing

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 
 export const CarrinhoContext = createContext();
+CarrinhoContext.displayName = 'Carrinho';
 
 export const CarrinhoProvider = ({children}) => {
     const [cart, setCart] = useState([]);
